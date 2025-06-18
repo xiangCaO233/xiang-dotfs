721950dd09b6e642e8ff0c89b5ad471135cd66e7
xiangCaO233 <odokoei@outlook.com>
2025-06-18 15:49:38 +0800

    适配macos

--- .bashrc
+++ .bashrc
@@ -9,3 +9,4 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 . "$HOME/.cargo/env"
+export PATH="/opt/homebrew/bin:$PATH"
--- wezterm/config/launch.lua
+++ wezterm/config/launch.lua
@@ -19,7 +19,7 @@ if platform.is_win then
       },
    }
 elseif platform.is_mac then
-   options.default_prog = { 'fish', '-l' }
+   options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
       { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
