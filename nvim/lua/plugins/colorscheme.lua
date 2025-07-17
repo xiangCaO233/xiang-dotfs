39453fb318d77502974eaf2b066ca39b23813459
xiangCaO233 <odokoei@outlook.com>
2025-07-17 10:08:25 +0800

    更新nvim配置

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
 	priority = 1000, -- 确保它在所有UI插件之前加载
 	config = function()
 		require("catppuccin").setup({
-			flavour = "mocha", -- 或者 "latte", "frappe", "mocha"
+			flavour = "frappe", -- 或者 "latte", "frappe", "mocha"
 
 			-- 这是实现你想要效果的关键！
 			transparent_background = true,
