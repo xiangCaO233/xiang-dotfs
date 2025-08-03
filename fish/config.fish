52413060a33708688807f348a27194677889d402
xiangCaO233 <odokoei@outlook.com>
2025-08-03 18:09:04 +0800

    修复不知道什么问题

--- fish/config.fish
+++ fish/config.fish
@@ -21,6 +21,7 @@ if status is-interactive
   set fish_cursor_default block      # Normal 模式：块状光标
   set fish_cursor_insert line        # Insert 模式：线状光标
   set fish_cursor_replace underscore # Replace 模式：下划线光标
+
 	function fish_user_key_bindings
     fish_vi_key_bindings  # 启用 Vi 模式
 
@@ -28,11 +29,14 @@ if status is-interactive
     bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 		# q 跳到上一个单词的末尾
 		# bind -M default q 'commandline -f backward-word; commandline -f forward-char'
-		bind -M default H beginning-of-line
+	bind -M default '^' beginning-of-line
+    bind -M default '$' end-of-line
+	bind -M default H beginning-of-line
     bind -M default L end-of-line
     bind -M default J 'commandline -f down-line; commandline -f down-line; commandline -f down-line; commandline -f down-line; commandline -f down-line'
     bind -M default K 'commandline -f up-line; commandline -f up-line; commandline -f up-line; commandline -f up-line; commandline -f up-line'
 
 	end
 	fish_user_key_bindings
+
 end
