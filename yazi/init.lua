564ea31eb23722762d4604b7df1da09dfdceb0ac
xiangCaO233 <odokoei@outlook.com>
2025-09-02 02:11:32 +0800

    adapt macos

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,8 +1,9 @@
--- require("full-border"):setup {
--- 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
--- 	type = ui.Border.ROUNDED,
--- }
--- 
+require("git"):setup()
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+})
+--
 function Linemode:size_and_mtime()
 	local time = math.floor(self._file.cha.mtime or 0)
 	if time == 0 then
@@ -39,10 +40,10 @@ Status:children_add(function()
 		return ""
 	end
 
-	return ui.Line {
+	return ui.Line({
 		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
 		":",
 		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
 		" ",
-	}
+	})
 end, 500, Status.RIGHT)
