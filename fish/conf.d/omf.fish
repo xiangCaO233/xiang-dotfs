1de9cde0240a1701efdd9449d26ebf297ffe27b2
xiangCaO233 <odokoei@outlook.com>
2026-01-12 23:57:35 +0800

    修复配置

--- fish/conf.d/omf.fish
+++ fish/conf.d/omf.fish
@@ -1,7 +1,4 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+# 只有当文件存在时才加载 Oh My Fish
+if test -f $HOME/.local/share/omf/init.fish
+    source $HOME/.local/share/omf/init.fish
+end
